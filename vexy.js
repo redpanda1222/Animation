a4fc5788a6b14ec25f578f0f3a3f1cea3733080a
redpanda1222 <soelin12usa@gmail.com>
2024-01-28 13:07:32 -0800

    frame count changed to vexy

--- vexy.js
+++ vexy.js
@@ -1,7 +1,7 @@
 class Vexy {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 900, 110, 120, 8, 0.1);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 900, 110, 120, 7, 0.1);
 
         this.x = 0;
         this.y = 450;
@@ -19,7 +19,7 @@ class Vexy {
 class Vexy1 {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 780, 110, 120, 8, 0.1);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 780, 110, 120, 7, 0.1);
 
         this.x = 150;
         this.y = 450;
@@ -37,7 +37,7 @@ class Vexy1 {
 class Vexy2 {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 650, 110, 120, 8, 0.1);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 650, 110, 120, 7, 0.1);
 
         this.x = 300;
         this.y = 450;
@@ -55,7 +55,7 @@ class Vexy2 {
 class Vexy3 {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 270, 110, 120, 8, 0.1);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 270, 110, 120, 7, 0.1);
 
         this.x = 450;
         this.y = 450;
@@ -73,7 +73,7 @@ class Vexy3 {
 class Vexy4 {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 400, 110, 120, 8, 0.1);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 400, 110, 120, 7, 0.1);
 
         this.x = 600;
         this.y = 450;
@@ -91,7 +91,7 @@ class Vexy4 {
 class Vexy5 {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 530, 110, 120, 8, 0.1);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 530, 110, 120, 7, 0.1);
 
         this.x = 750;
         this.y = 450;
@@ -109,7 +109,7 @@ class Vexy5 {
 class Vexy6 {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 1035, 110, 120, 8, 0.1);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./vexy.png"), 0, 1035, 110, 120, 7, 0.1);
 
         this.x = 900;
         this.y = 450;
